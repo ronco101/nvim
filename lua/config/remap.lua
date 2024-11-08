df32e238900919b57fda294a8449e589b7b5a89d
Ron <97910602+ABadReference@users.noreply.github.com>
2024-11-07 20:24:47 -0500

    Made a mistake
    I mistakenly add the root folder, whoops

---
+++ lua/config/remap.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.bo.softtabstop = 4
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,11 @@
+return {
+    {'VonHeikemen/lsp-zero.nvim',
+        branch = 'v4.x',
+    },
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+}
+
